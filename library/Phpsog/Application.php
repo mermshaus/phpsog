bfdfbb953c844133460d7e37a88573b8650d8986
Marc Ermshaus <marc@ermshaus.org>
2012-03-07 14:09:13 +0100

    Hopefully this will do it for Composer integration of the CLI script.

--- library/Phpsog/Application.php
+++ library/Phpsog/Application.php
@@ -3,14 +3,22 @@
  *
  */
 
-namespace org\ermshaus\phpsog;
+namespace Phpsog;
 
 use Kaloa\Filesystem\PathHelper;
+use UnexpectedValueException;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+function e($s)
+{
+    return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
 
 /**
  *
  */
-class PhpSog
+class Application
 {
     /** @var array */
     protected $config;
@@ -37,7 +45,7 @@ class PhpSog
 
         $projectDir = realpath(dirname($pathToConfig));
 
-        $defaultConfig = parse_ini_file('./config.default.ini');
+        $defaultConfig = parse_ini_file(__DIR__ . '/config.default.ini');
 
         $projectConfig = parse_ini_file($pathToConfig);
 
@@ -150,10 +158,18 @@ class PhpSog
     {
         $config = $this->config;
 
-        $dirIter = new \RecursiveDirectoryIterator($config['project.dir'] . '/' . $config['resources.dir']);
-        $recursiveIterator = new \RecursiveIteratorIterator($dirIter,
-            \RecursiveIteratorIterator::SELF_FIRST,
-            \RecursiveIteratorIterator::CATCH_GET_CHILD);
+        $dirIter = null;
+
+        try {
+            $dirIter = new RecursiveDirectoryIterator($config['project.dir'] . '/' . $config['resources.dir']);
+        } catch (UnexpectedValueException $e) {
+            echo '  Found no resource directory.' . PHP_EOL;
+            return;
+        }
+
+        $recursiveIterator = new RecursiveIteratorIterator($dirIter,
+            RecursiveIteratorIterator::SELF_FIRST,
+            RecursiveIteratorIterator::CATCH_GET_CHILD);
 
         foreach ($recursiveIterator as $file => $unused) {
 
